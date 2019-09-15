9ed5e9aff40c90e4cf0078cd82ff552b43ba0350
lciamp <l.ciamp@yahoo.com>
2019-09-15 17:05:55 -0400

    fixed unused import on resources/driver

--- api/resources/driver.py
+++ api/resources/driver.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource, marshal
 from flask_jwt_extended import (
     jwt_required,
-    get_jwt_claims,
     jwt_optional,
     get_jwt_identity,
     fresh_jwt_required,
